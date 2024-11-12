1a7c86b4e491f0f4ec8dd394ef408212f600c3a2
Harsh Choudhary <hc03092002@gmail.com>
2024-11-12 12:57:17 +0530

    database_scripts

---
+++ utils/database scripts/insertintosql.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import pymysql
+
+# Connect to MySQL database
+connection = pymysql.connect(
+    host = "localhost",
+    user = "root",
+    password = "root",
+    database = "python_training_management_database"
+)
+
+
+# Read Excel file
+df = pd.read_csv(r"C:\Users\harsh.choudhary\Desktop\python_management\teamdata.csv")
+
+# Insert DataFrame into MySQL table
+for index, row in df.iterrows():
+    sql_query = "INSERT INTO Training_data VALUES (%s, %s)"
+    values = tuple(row)
+    with connection.cursor() as cursor:
+        cursor.execute(sql_query, values)
+        connection.commit()
+
+connection.close()
+
+print("Data inserted successfully!")
